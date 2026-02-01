980cc93ce8b1322d2f2536a8d8c5b964f32002ce
Kangsanboy <128477621+Kangsanboy@users.noreply.github.com>
2026-02-01 09:20:16 +0700

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,11 +3,15 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://elfqgpegkcbrizccrzbv.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsZnFncGVna2Nicml6Y2NyemJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MjAzNzksImV4cCI6MjA4MDE5NjM3OX0.QRRssTF0vUEH6TvErnd15uMhs3uiUGJ00xUK7VSKbFw'
 
-if (!supabaseUrl || !supabaseKey) {
-  console.error("🚨 BAHAYA: Supabase URL atau Key tidak ditemukan! Cek file .env atau Vercel Settings.");
-}
+console.log("Mencoba connect ke:", supabaseUrl); // Cek di console nanti muncul gak
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);{
+    auth: {
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: true
+    }
+});
 
 // Import the supabase client like this:
 // For React:
